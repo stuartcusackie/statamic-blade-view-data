67fb0f4b9d7acbf0af66f8fcf21e45889ef8131a
Stuart Cusack <stuart@stuartcusack.ie>
2022-07-03 19:08:44 +0100

    Remove nav config

--- config/statamic-blade-view-data.php
+++ config/statamic-blade-view-data.php
@@ -12,27 +12,7 @@ return [
     */
 
     'views' => [
-        'pages.*',
-        'blog.*'
-    ],
-    
-    /*
-    |--------------------------------------------------------------------------
-    | Navs
-    |--------------------------------------------------------------------------
-    |
-    | Add all nav handles that you want to be able to access from your views.
-    | You can also set the specific nav params that you require, and enabled
-    | caching for better performance. Remember, each nav adds processing time
-    | and cached navs should be forgetten using an Event Service Provider.
-    |
-    */
-
-    'navs' => [
-        'main' => [
-            'select' => 'children|title|url|is_current|hide_from_header',
-            'cache' => true
-        ]
+        'pages.*'
     ]
 
 ];
