88adb084b2490952171726940a1690a0806f47ae
Stuart Cusack <stuart@stuartcusack.ie>
2022-08-10 14:19:57 +0100

    Use request path to determine current template

--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -27,29 +27,28 @@ class ServiceProvider extends AddonServiceProvider
         return $this;
     }
 
-
+    /**
+     * Process the view data that has been set up
+     * by Statamic. This is bit convoluted but
+     * it's efficient!
+     */
     protected function registerViewComposers()
     {
-        $views = [];
+        if($entry = Entry::findByUri('/' . request()->path())) {
 
-        /**
-         * This may not be the most efficient way
-         * espcially if we have lots of entries.
-         * Can we get all templates from blueprints
-         * instead??
-         */
-        foreach(Entry::all() as $entry) {
-
-            if(!in_array($entry->template(), $views)) {
-                $views[] = $entry->template();
-            }
+            View::composer($entry->template(), function ($view) {
+                \StatData::init($view->getData());
+            });
 
         }
+        else if($term = Term::findByUri('/' . request()->path())) {
 
-        View::composer($views, function ($view) {
-            \StatData::init($view->getData());
-        });
+            View::composer($term->template(), function ($view) {
+                \StatData::init($view->getData());
+            });
 
+        }
+        
         return $this;
     }
 }
